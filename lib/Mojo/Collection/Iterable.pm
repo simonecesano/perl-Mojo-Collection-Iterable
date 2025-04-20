d229ed2a631fa8b874bd434a9d368d667c403697
Simone Cesano <simonecesano@libero.it>
2025-04-20 17:49:41 +0200

    Multiple fixes, documentation

--- lib/Mojo/Collection/Iterable.pm
+++ lib/Mojo/Collection/Iterable.pm
@@ -1,5 +1,4 @@
 package Mojo::Collection::Iterable;
-use Mojo::Util qw/dumper/;
 use Mojo::Base -base;
 
 use Mojo::Collection;
@@ -7,6 +6,7 @@ use Scalar::Util qw/blessed/;
 use Class::Method::Modifiers;
 
 # ABSTRACT: turns baubles into trinkets
+use List::Util;
 
 use overload
     '@{}' => sub { shift->collection },
@@ -40,27 +40,28 @@ sub BEGIN {
 		     join
 		     last
 		     map
-		     reduce
 		     reverse
 		     shuffle
 		     size
-		     sort
 		     tail
 		     tap
-		     to_array
-		     uniq
 		 /;
     no strict 'refs';
+
     foreach my $name (@subs) {
 	local $\ = "\n";
 	# next unless defined &$name;
 	# my $new_name = '' . $name;
 	# print STDERR join "::",  __PACKAGE__, "$name";
+
 	no strict "refs"; 
+
 	*{join "::",  __PACKAGE__, "$name"} = sub {
 	    my $self = shift;
 
 	    my $ret = $self->collection->$name(@_);
+
+	    print ref $self, $name, $ret, @_ if $name eq "reduce";
 
 	    if ((blessed $ret) && ($ret->isa("Mojo::Collection") || ref $ret eq "ARRAY")) {
 		return (ref $self)->new($ret->@*);
@@ -71,25 +72,74 @@ sub BEGIN {
     }
 }
 
-sub prev { $_[0]->idx > 0 ? $_[0]->collection->[$_[0]->idx - 1] : undef }
-sub curr { $_[0]->collection->[$_[0]->idx] }
-sub item { $_[0]->collection->[$_[0]->idx] }
-sub next { $_[0]->collection->[$_[0]->idx + 1] }
+# --------------------------------------------
+# these had to be defined explicitly here
+# otherwise the whole thing would break
+# I don't remember why but it did
+# probably deep recursion
+# --------------------------------------------
 
-sub increment { $_[0]->idx($_[0]->idx + 1) }
-sub decrement { $_[0]->idx($_[0]->idx - 1) }
-sub reset { $_[0]->idx(0) }
-
-sub iterate {
+sub reduce {
     my $self = shift;
-    my $ret = $self->collection->[$self->idx];
-    $self->increment;
-    return $ret;
+    @_ = (@_, $self->collection->@*);
+    goto &List::Util::reduce;
+}
+
+sub sort {
+    my ($self, $cb) = @_;
+    return $self->new(sort @$self) unless $cb;
+
+    my $caller = caller;
+    no strict 'refs';
+
+    my @sorted = sort {
+	local (*{"${caller}::a"}, *{"${caller}::b"}) = (\$a, \$b);
+	$a->$cb($b);
+    } @$self;
+    return $self->new(@sorted);
+}
+
+sub uniq {
+    my ($self, $cb) = (shift, shift);
+    my %seen;
+    return $self->new(grep { !$seen{$_->$cb(@_) // ''}++ } $self->collection->@*) if $cb;
+    return $self->new(grep { !$seen{$_ // ''}++ } $self->collection->@*);
+}
+
+sub to_array {
+    my $self = shift;
+    return $self->collection->to_array
 }
 
 
+# --------------------------------------------
+
+
+sub prev { $_[0]->idx > 0 ? $_[0]->collection->[$_[0]->idx - 1] : undef }
+sub curr { $_[0]->collection->[$_[0]->idx] }
+sub current { $_[0]->collection->[$_[0]->idx] }
+sub item { $_[0]->collection->[$_[0]->idx] }
+sub next { $_[0]->idx < (-1 + scalar $_[0]->@*) ?  $_[0]->collection->[$_[0]->idx + 1] : undef }
+
+sub reset { $_[0]->idx(0) }
+
+sub increment { $_[0]->idx($_[0]->idx + 1) if $_[0]->idx < -1 + scalar $_[0]->@*; return $_[0] }
+sub decrement { $_[0]->idx($_[0]->idx - 1) if $_[0]->idx > 0; return $_[0] }
+
+{
+    my $i = 0;
+    sub iterate {
+	my $self = shift;
+	my $ret = $self->collection->[$i];
+	if ($i < scalar $self->@*) { $self->idx($i) };
+	$i++;
+	return $ret
+    }
+}
+
 sub _ref { ref $_[0] eq 'ARRAY' || blessed $_[0] && $_[0]->isa(__PACKAGE__) }
 sub _flatten { map { _ref($_) ? _flatten(@$_) : $_ } @_ }
+
 sub flatten { $_[0]->new(_flatten(@{$_[0]})) }
 sub with_roles { shift->Mojo::Base::with_roles(@_) }
 
@@ -114,14 +164,6 @@ sub watch {
 
     push $self->{watches}->@*, [ $when => $sub => $name ];
 }
-
-
-sub from {
-    my $self = shift;
-    my $start = shift;
-    return $self->new(@{$self}[$start..$self->idx])
-}
-
 
 1;
 
@@ -152,27 +194,159 @@ Mojo::Collection - Collection
 
 =head1 DESCRIPTION
  
-L<Mojo::Collection::Iterable> adds an iterator function to Mojo::Collection and makes it subclassable via Mojo::Base as a side-effect
+=encoding utf8
+
+=head1 NAME
+
+Mojo::Collection::Iterable - Collection with iterator capabilities
+
+=head1 SYNOPSIS
+
+  use Mojo::Collection::Iterable;
+
+  # Create a new collection
+  my $collection = Mojo::Collection::Iterable->new(qw(it just works));
+
+  # Use like an arrayref
+  unshift @$collection, 'Indeed';
+  say $collection->join("\n");
+
+  # Chainable methods
+  $collection->map(sub { ucfirst })->shuffle->each(sub ($word, $num) {
+    say "$num: $word";
+  });
+
+  # Iterate manually
+  $collection->reset;
+  while (my $item = $collection->iterate) {
+    say "Item: $item"; # returns "it"
+  }
+
+=head1 DESCRIPTION
+
+L<Mojo::Collection::Iterable> is a subclassable extension to L<Mojo::Collection> that adds iterator-style access and extra utilites.
 
 =head1 ATTRIBUTES
 
 =head2 idx
 
+  $int = $collection->idx;
+  $collection->idx($int);
+
+Sets/gets thecurrent position of the iterator in the collection.
+
 =head2 collection
+
+  $mc = $collection->collection;
+
+The internal L<Mojo::Collection> object that stores the actual items.
+
+=head2 watches
+
+  $watches = $collection->watches;
+
+An arrayref of watch conditions stored as triples `[when => sub => name]`.
 
 =head1 METHODS
 
+=head2 new
+
+  my $collection = Mojo::Collection::Iterable->new(@items);
+
+Creates a new collection with the given list of items.
+
 =head2 prev
+
+  my $item = $collection->prev;
+
+Returns the item before the current index, or C<undef> if at the beginning.
 
 =head2 curr
 
+=head2 item
+
+  my $item = $collection->curr;
+  my $item = $collection->item;
+
+Returns the item at the current index.
+
 =head2 next
+
+  my $item = $collection->next;
+
+Returns the item after the current index, or C<undef> if at the end.
 
 =head2 increment
 
-=head2 iterate
+  $collection->increment;
+
+Increments the iterator index by 1.
+
+=head2 decrement
+
+  $collection->decrement;
+
+Decrements the iterator index by 1.
 
 =head2 reset
 
+  $collection->reset;
 
+Resets the index to the start (0).
 
+=head2 iterate
+
+  my $item = $collection->iterate;
+
+Returns the current item starting from the first, then increments the index.
+
+=head2 watch
+
+  $collection->watch($when => $callback, $name);
+
+Adds a watch hook (currently not invoked in the provided code, but may be used for event handling or hooks).
+
+=head1 OVERLOADING
+
+This class overloads the following operators:
+
+=over 4
+
+=item *
+
+C<@{}> — dereference as an array.
+
+=item *
+
+C<""> — stringification returns the object (could be customized further).
+
+=item *
+
+C<${}> — scalar dereference returns the object.
+
+=item *
+
+C<++> — calls C<increment>.
+
+=item *
+
+C<--> — calls C<decrement>.
+
+=back
+
+=head1 SEE ALSO
+
+L<Mojo::Collection>, L<Mojo::Base>, L<List::Util>, L<Scalar::Util>
+
+=head1 AUTHOR
+
+Simone Cesano <scesano@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) Simone Cesano.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
